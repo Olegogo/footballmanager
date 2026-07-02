647d4987566d3b616d552411e5033d1f60394e66
Oleg <olegkoreshkov53@gmail.com>
2026-07-02 20:02:34 +0300

    Refine game field spacing

--- web/app.js
+++ web/app.js
@@ -2324,7 +2324,7 @@ function renderField(game, options = {}) {
     <section class="panel field-panel field-panel--static ${options.className ? escapeHtml(options.className) : ''}">
       <div class="field">
         <div class="field-image field-image--top" aria-hidden="true">
-          <img src="/assets/field/field-pull-down-topview-1200.webp" alt="">
+          <img src="/assets/field/field-pull-down-topview-cropped-clean.webp" alt="">
         </div>
         ${emptyMessage ? `<div class="field-empty">${escapeHtml(emptyMessage)}</div>` : ''}
         <div class="field-player-layer">
